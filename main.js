d576ed484b62ecb422293af8afba74838d5af6cf
Nurmurod12 <147878276+Nurmurod12@users.noreply.github.com>
2023-10-20 09:15:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+// //problem_180
+
+// function function1() {
+//     console.log(Math.max(10))
+//     console.log(Math.min(1))
+// }
+
+// function function2(){
+//     var number = [2,10,5,8,12]
+//     console.log(function2(Math.min(2), Math.max(12)))
+// }
+// function2()
+
+
+// function function3(){
+//     var son = [9,-14,3,2,1]
+//     console.log(function3(Math.min(-14), Math.max(9)))
+// }
+// function3()
+
+
+//problem_155
+
+
+// var raqam = parseInt(2, 6, 9, 7, 5)
+
+// console.log(raqam.shift());
+// console.log(raqam[1]);
+// console.log(raqam.pop());
+
+
+//  problem_167
+
+// var qiymat =[12, 24, 34, 23, 23, 4, 224]
+// var sum = 0
+
+// for(var i = 0; i < qiymat.length; i++){
+//     if(i % 2 == 0){
+//         sum == qiymat[i];
+//     }
+// }
+// console.log(sum);
